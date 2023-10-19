0e06724149b5ad9bd869be45cbd7480b1b7760e2
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2023-10-18 23:05:13 -0400

    Update loginPage.test.js

--- loginPage.test.js
+++ loginPage.test.js
@@ -1,1 +1,23 @@
+const request = require('supertest');
+const app = require('./app'); 
+
+describe('Login Route Tests', () => {
+  it('should return 302 or 200 and a success message for valid login', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({ emailInput: 'testdev@gmail.com', passwordInput: '1911194@Sweet' });
+
+      expect(response.status === 302 || response.status === 200).toBe(true);
+    
+  });
+
+  it('should return 400 and an error message for invalid login', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({ emailInput: 'testuser', passwordInput: 'wrongpassword' });
+
+    expect(response.status).toBe(400);
+    
+  });
+});
 
