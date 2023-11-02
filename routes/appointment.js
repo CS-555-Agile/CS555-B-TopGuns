bc2e015d5dde9dbd6d75823f2bbbbbbcb34958ce
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2023-11-02 11:35:08 -0400

    Update appointment.js

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { appointments} = require("../config/mongoCollections");
 const {
-  createAppointments
+  createAppointments,
+  getUserByconsultant,
+  getUserByprofessionalStaff,
+  getPastAppointment,
+  getupComingAppointment
 } = require("../data/appointments");
 const {
   validObjectId,
@@ -12,10 +16,47 @@ const xss = require("xss");
 const { ObjectId } = require("mongodb");
 
 
-
-router.route("/").post(async (req, res) => {
+router.route("/").get(async (req, res) => {
+  //code here for GET
+  try {
+    if (!req.session.user?.verified || req.session.user.category!="patient") {
+      return res.redirect("/home");
+    } 
+    else {
+      let userId = String(req.session.user._id);
+      console.log(userId)
+     
+      const past = await getPastAppointment(userId);
+      const future = await getupComingAppointment(userId);
+      console.log(past)
+      console.log(future)
+     
+        
+      return res
+        .status(200)
+        .render("appointment/book", {
+          past:past,
+          future:future,
+          title: "Book appointment",
+          partial: "book-script",
+          css: "book-css",
+          
+        });
+      
+    }
+  } catch (err) {
+    return res
+      .status(err?.status ?? 500)
+      .render("auth/signup", {
+        title: "Login",
+        partial: "signup-script",
+        css: "signup-css",
+      });
+  }
+})
+.post(async (req, res) => {
   console.log("patient_id")
-  // const patient_id = req.session.user._id
+  
   // console.log(patient_id)
 
   // try {
@@ -34,29 +75,32 @@ router.route("/").post(async (req, res) => {
   try {
     let {
           category,
-          consultant_id,
+          sub_category,
           time_slot,
+          date,
           notes,
         } = req.body; 
+    let patient_id = String(req.session.user._id)
     category = xss(category);
-    consultant_id = xss(consultant_id);
+    consultant_id = xss(String(sub_category));
     time_slot = xss(time_slot);
     notes = xss(notes);
-    
-    let patient_id = "3454566767ebavd"
+    date = xss(date);
+    // let patient_id = "3454566767ebavd"
     const newAppointment = await createAppointments(
       category,
       consultant_id,
       patient_id,
       time_slot,
+      date,
       notes
     );
     if (!newAppointment.bookedAppointment) {
       console.log(err, "Line 194");
       return res.status(err?.status ?? 500)
     } else {
-      // return res.redirect("/home");
-      return res.json("inserted");
+      return res.redirect("/home");
+      
     }
   } catch (err) {
     console.log(err, "Line 206");
@@ -66,6 +110,33 @@ router.route("/").post(async (req, res) => {
 
 
 });
+router.route("/getConsultant").get(async (req, res) => {
+  try {
+    if (!req.session.user?.verified) {
+      return res.redirect("/home");
+    } else {
+       const data = await getUserByconsultant();
+      return res.json(data)
+    }
+  } catch (err) {
+    return res.json(err)
+  }
 
+
+});
+router.route("/getProfessioanl").get(async (req, res) => {
+  try {
+    if (!req.session.user?.verified) {
+      return res.redirect("/home");
+    } else {
+       const data = await getUserByprofessionalStaff();
+      return res.json(data)
+    }
+  } catch (err) {
+    return res.json(err)
+  }
+
+
+});
 module.exports = router;
 
