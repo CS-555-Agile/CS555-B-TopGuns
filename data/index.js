4e6f336b2e0939124b446736e445e77cd9bed16b
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2023-12-06 14:57:34 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,19 +1,10 @@
-const authRoutes = require("./authRoutes");
-const homeRoutes= require('./home');
-const appointmentsRoutes= require('./appointment');
-const feedbackRoutes = require('./feedbacks');
-const blogRoutes = require('./blog');
-const entertainmentRoutes = require('./entertainment');
-
-const constructorMethod = (app) => {
-  app.use('/', authRoutes);
-  app.use('/home', homeRoutes);
-  app.use('/appointment', appointmentsRoutes);
-  app.use('/feedbacks', feedbackRoutes);
-  app.use('/blog', blogRoutes);
-  app.use('/entertainment', entertainmentRoutes);
-  app.use('*', (req, res) => {
-    res.status(404).send("Page not found");
-  });
+users = require("./users");
+appointments = require("./appointments");
+feedbacks = require("./feedbacks");
+post = require("./post");
+module.exports = {
+    users:users,
+    appointments:appointments,
+    feedbacks: feedbacks,
+    post:post
 };
-module.exports = constructorMethod;
