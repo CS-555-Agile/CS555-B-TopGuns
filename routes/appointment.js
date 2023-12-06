4a94f243c966444f8bfd6d3bd803814c5a7b7649
Shiv Shah <112986630+ShivShah202000@users.noreply.github.com>
2023-12-06 14:00:31 -0800

    Update appointment.js

--- routes/appointment.js
+++ routes/appointment.js
@@ -23,7 +23,7 @@ const { ObjectId } = require("mongodb");
 router.route("/").get(async (req, res) => {
   //code here for GET
   try {
-    if (!req.session.user?.verified || req.session.user.category!="patient") {
+    if (!req.session.user.verified || req.session.user.category!="patient") {
       return res.render("home/homePage",{
         title: "Home",
         partial: "home-script",
@@ -55,7 +55,7 @@ router.route("/").get(async (req, res) => {
     }
   } catch (err) {
     return res
-      .status(err?.status ?? 500)
+      .status(err && err.status?err.status:500)
       .render("auth/signup", {
         title: "Login",
         partial: "signup-script",
@@ -106,7 +106,7 @@ router.route("/").get(async (req, res) => {
     );
     if (!newAppointment.bookedAppointment) {
       console.log(err, "Line 194");
-      return res.status(err?.status ?? 500)
+      return res.status(err && err.status?err.status:500)
     } else {
       if(req.session.user.category ==="patient"){
           return res.render("home/homePage",{
@@ -128,7 +128,7 @@ router.route("/").get(async (req, res) => {
     }
   } catch (err) {
     console.log(err, "Line 206");
-    return res.status(err?.status ?? 500)
+    return res.status(err && err.status?err.status:500)
     
   }
 
@@ -232,7 +232,7 @@ router.route("/show").get(async (req, res) => {
     }
   } catch (err) {
     return res
-      .status(err?.status ?? 500)
+      .status(err && err.status?err.status:500)
       .render("auth/signup", {
         title: "Login",
         partial: "signup-script",
