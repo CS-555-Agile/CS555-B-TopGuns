a9cc04f0055a69ec2f0f699f2d7cc2e006ad77b1
Vaishvik Brahmbhatt <36341195+vaishvik24@users.noreply.github.com>
2023-12-06 13:33:27 -0600

    New appointment changes after refactoring

--- data/appointments.js
+++ data/appointments.js
@@ -49,7 +49,10 @@ const getPendingAppointment = async (userId) => {
   const appointmentCollection = await appointments();
   const userObject = await appointmentCollection.find( { consultant_id: (userId), date: {$gte: dateString}, status:"Pending" }).toArray();
   if (userObject === null)
-    throw new notFoundError("No appointments found with this ID");
+    throw {
+      message: "No appointments found with this ID!",
+      code: 404,
+    };
   return userObject;
 };
 const getPast = async (userId) => {
@@ -59,7 +62,10 @@ const getPast = async (userId) => {
   const appointmentCollection = await appointments();
   const userObject = await appointmentCollection.find( { consultant_id: (userId), date: {$lt: dateString}, status: { $ne: "Pending" } }).toArray();
   if (userObject === null)
-    throw new notFoundError("No appointments found with this ID");
+    throw {
+      message: "No appointments found with this ID!",
+      code: 404,
+    };
   return userObject;
 };
 const getupComing = async (userId) => {
@@ -68,7 +74,10 @@ const getupComing = async (userId) => {
   const appointmentCollection = await appointments();
   const userObject = await appointmentCollection.find( { consultant_id: (userId), date: {$gte: dateString}, status: { $ne: "Pending" } }).toArray();
   if (userObject === null)
-    throw new notFoundError("No appointments found with this ID");
+    throw {
+      message: "No appointments found with this ID!",
+      code: 404,
+    };
   return userObject;
 };
 
@@ -80,7 +89,10 @@ const getPastAppointment = async (userId) => {
   const appointmentCollection = await appointments();
   const userObject = await appointmentCollection.find( { patient_id: (userId), date: {$lt: dateString} }).toArray();
   if (userObject === null)
-    throw new notFoundError("No appointments found with this ID");
+    throw {
+      message: "No appointments found with this ID!",
+      code: 404,
+    };
   return userObject;
 };
 const getupComingAppointment = async (userId) => {
@@ -89,7 +101,10 @@ const getupComingAppointment = async (userId) => {
   const appointmentCollection = await appointments();
   const userObject = await appointmentCollection.find( { patient_id: (userId), date: {$gte: dateString} }).toArray();
   if (userObject === null)
-    throw new notFoundError("No appointments found with this ID");
+    throw {
+      message: "No appointments found with this ID!",
+      code: 404,
+    };
   return userObject;
 };
 const getUserByprofessionalStaff = async () => {
@@ -97,7 +112,10 @@ const getUserByprofessionalStaff = async () => {
   const userCollection = await users();
   const userObject = await userCollection.find({ category:"professionalStaff" }).toArray();
   if (userObject === null)
-    throw new notFoundError("No Staff found with this ID");
+    throw {
+      message: "No user found as professional staff!",
+      code: 404,
+    };
   return userObject;
 };
 const getUserByconsultant = async () => {
@@ -105,21 +123,17 @@ const getUserByconsultant = async () => {
   const userCollection = await users();
   const userObject = await userCollection.find({ category:"consultant" }).toArray();
   if (userObject === null)
-    throw new notFoundError("No Consultant found with this ID");
+    throw {
+      message: "No user found as consultant !",
+      code: 404,
+    };
   return userObject;
 };
-
-function MissingFieldsError(message) {
-  this.name = 'MissingFieldsError';
-  this.message = message || 'All fields must be supplied!';
-  this.stack = new Error().stack;
-}
-
 
 const createAppointments = async (category, consultant_id, patient_id, time_slot, date,notes) => {
   // Validations
   try {
-    if (!category || !consultant_id || !patient_id || !time_slot || !date) throw new MissingFieldsError();
+    if (!category || !consultant_id || !patient_id || !time_slot || !date) throw `All fields must be supplied!`;
     //add validations
     //check time
 
@@ -144,13 +158,17 @@ const createAppointments = async (category, consultant_id, patient_id, time_slot
     };
 
     const insertInfo = await appointmentsCollection.insertOne(newAppointments);
-    if (!insertInfo.acknowledged || !insertInfo.insertedId)
-      throw new internalServerError("Could not book appointment");
-  
+    if (!insertInfo.acknowledged || !insertInfo.insertedId){
+      throw internalServerError("Could not book appointment");
+    }
+    else{
+      
     return {bookedAppointment: true};
+    }
+  
   } 
   catch (err) {
-    throw new err;
+    throw err;
   }
 };
 
