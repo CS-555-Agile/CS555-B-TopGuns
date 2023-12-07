b1305273cd680a3122cbd8ce27837d289e39e26a
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2023-12-06 19:11:02 -0500

    Add files via upload

--- data/feedbacks.js
+++ data/feedbacks.js
@@ -1,5 +1,6 @@
 const { ObjectId } = require("mongodb");
 const { feedbacks } = require("../config/mongoCollections");
+const { users } = require("../config/mongoCollections");
 const { badRequestError, internalServerError, notFoundError } = require("../helpers/wrappers");
 const {validString, validObjectId } = require("../helpers/validations");
 
@@ -7,22 +8,45 @@ const getFeedbacksPatients = async (patientId) => {
     try {
         let feedbacksCollection = await feedbacks();
         let feedbacksList = await feedbacksCollection.find({patientId: patientId}).toArray();
-        
+        // console.log(feedbacksList);
         return feedbacksList;
     } catch (err) {
-        
+        // console.log(err);
         throw (err);
     }
 }
-
+function singleDigitToList(digit) {
+    const list = [];
+    for (let i = 0; i < digit; i++) {
+      list.push(digit);
+    }
+    return list;
+  }
 const getFeedbacksStaff = async (staffId) => {
     try {
         let feedbacksCollection = await feedbacks();
         let feedbacksList = await feedbacksCollection.find({staffId: staffId}).toArray();
-        
+        for (data of feedbacksList) {
+            const userCollection = await users();
+            const userObject = await userCollection.findOne({ _id: ObjectId(data.patientId) });
+            if (userObject === null)
+            throw {
+            message: "No user with this ID can be found!",
+            code: 404,
+            };
+            else{
+                const num = Number(data.rating);
+                const empty = 5 - num
+                const list = singleDigitToList(num);
+                data.ratings = list
+                data.empty = singleDigitToList(empty)
+                data.patientName = userObject.firstName
+                data.patientEmail = userObject.email
+            }
+          }
         return feedbacksList;
     } catch (err) {
-        
+        // console.log(err);
         throw (err);
     }
 }
@@ -30,15 +54,15 @@ const getFeedbacksStaff = async (staffId) => {
 const createFeedback = async (feedbackObject) => {
     // Validations
     try {
-        if (!feedbackObject.staffId) throw new Error(`Staff Id is mandatory field.`);
-        if (!feedbackObject.patientId) throw new Error(`Patient Id is mandatory field.`);
-        if (!feedbackObject.subject) throw new Error(`Subject/Title is mandatory field.`);
-        if (!feedbackObject.body) throw new Error(`Body is mandatory field.`);
-        if (!feedbackObject.rating) throw new Error(`Rating is mandatory field.`);
-        if (!feedbackObject.timestamp) throw new Error(`Timestamp is mandatory field.`);
+        if (!feedbackObject.staffId) throw `Staff Id is mandatory field.`;
+        if (!feedbackObject.patientId) throw `Patient Id is mandatory field.`;
+        if (!feedbackObject.subject) throw `Subject/Title is mandatory field.`;
+        if (!feedbackObject.body) throw `Body is mandatory field.`;
+        if (!feedbackObject.rating) throw `Rating is mandatory field.`;
+        if (!feedbackObject.timestamp) throw `Timestamp is mandatory field.`;
 
         if(!(feedbackObject.rating >= 0 && feedbackObject.rating <= 10)) {
-          throw new Error(`Rating has to be in scale of 1-10`);
+          throw `Rating has to be in scale of 1-10`;
         }
         // Trim inputs
         feedbackObject.staffId = feedbackObject.staffId.trim();
@@ -46,23 +70,20 @@ const createFeedback = async (feedbackObject) => {
         feedbackObject.subject = feedbackObject.subject.trim();
         feedbackObject.body = feedbackObject.body.trim();
         feedbackObject.rating = feedbackObject.rating.trim();
-        //feedbackObject.timestamp = feedbackObject.timestamp;
+        feedbackObject.timestamp = feedbackObject.timestamp;
   
         const feedbacksCollection = await feedbacks();
 
+        // console.log(feedbackObject);
         const insertInfo = await feedbacksCollection.insertOne(feedbackObject);
-        try {
-            if (!insertInfo.acknowledged || !insertInfo.insertedId) {
-                throw internalServerError("Failed to add give feedback.");
-            }
-            return { insertedFeedback: true };
-        } catch (err) {
+        if (!insertInfo.acknowledged || !insertInfo.insertedId)
+            throw internalServerError("Failed to add give feedback.");
+            return {insertedFeedback: true};
+        } 
+        catch (err) {
             console.log("Error while creating feedback: " + err);
             throw err;
         }
-        
-
-        
 }
 
 module.exports = {
